cb138d158b21f0f8d8e10eb9bec0a8ccff1bc77e
tingyik90 <tingyik90@gmail.com>
2019-01-07 22:03:54 +0800

    v2.3.2 - Rearranged code to allow debugging in Application.

--- app/src/main/java/com/dokidevs/pholder/PholderApplication.kt
+++ app/src/main/java/com/dokidevs/pholder/PholderApplication.kt
@@ -5,6 +5,7 @@ import com.crashlytics.android.Crashlytics
 import com.crashlytics.android.core.CrashlyticsCore
 import com.dokidevs.dokilog.DokiLog
 import com.dokidevs.dokilog.LogProfile
+import com.dokidevs.dokilog.d
 import com.dokidevs.pholder.data.PholderDatabase
 import com.dokidevs.pholder.service.FileIntentService
 import com.dokidevs.pholder.utils.ColorUtils
@@ -15,7 +16,7 @@ import com.squareup.leakcanary.LeakCanary
 import io.fabric.sdk.android.Fabric
 
 /*--- PholderApplication ---*/
-class PholderApplication : Application() {
+class PholderApplication : Application(), DokiLog {
 
     /* companion object */
     companion object {
@@ -33,20 +34,14 @@ class PholderApplication : Application() {
 
     }
 
+    // onCreate
     override fun onCreate() {
         super.onCreate()
-        // Set default preferences and get prefManager
-        prefManager = PrefManager(applicationContext)
-        isDarkTheme = prefManager.get(PREF_IS_DARK_THEME, true)
-        animateGif = prefManager.get(PREF_ANIMATE_GIF, true)
-        pholderDatabase = PholderDatabase.getInstance(applicationContext)
-        colorUtils = ColorUtils(applicationContext, isDarkTheme)
-        // Initiate database
-        FileIntentService.initialise(applicationContext)
         // For debug
         if (BuildConfig.DEBUG) {
             DokiLog.addProfile(LogProfile("Debug"))
         }
+        d()
         // Set up Crashlytics, disabled for debug builds.
         // This is not really required, proguard will remove some critical methods in firebase without this.
         // This will crash app at launch.
@@ -59,5 +54,14 @@ class PholderApplication : Application() {
             return
         }
         LeakCanary.install(this)
+        // Get prefManager and set preferences
+        prefManager = PrefManager(applicationContext)
+        isDarkTheme = prefManager.get(PREF_IS_DARK_THEME, true)
+        animateGif = prefManager.get(PREF_ANIMATE_GIF, true)
+        colorUtils = ColorUtils(applicationContext, isDarkTheme)
+        // Initiate database
+        pholderDatabase = PholderDatabase.getInstance(applicationContext)
+        FileIntentService.initialise(applicationContext)
     }
+
 }
